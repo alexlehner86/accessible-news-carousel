e4888203285ef57aabe3b50b9e5dc3025e4a474d
Alexander Lehner <alexlehner86@gmail.com>
2023-01-07 19:36:43 +0100

    create prev and next buttons

--- src/app/components/news-carousel/news-carousel.component.ts
+++ src/app/components/news-carousel/news-carousel.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostBinding, Input } from '@angular/core';
 
-import { NewsCarouselItem } from './news-carousel.interface';
+import { NewsCarouselConfig, NewsCarouselItem } from './news-carousel.interface';
 
 /**
  * TODO: describe component
@@ -19,23 +19,37 @@ export class NewsCarouselComponent {
      * The aria label for the carousel container.
      * It should convey to screenreader users what the carousel is about.
      */
-    @HostBinding('attr.aria-label') @Input() carouselLabel!: string;
-    /**
-     * The role description of the carousel container. Default: "carousel"
-     */
-    @HostBinding('attr.aria-roledescription') @Input() carouselDescription = 'carousel';
-    /**
-     * The role description for each slide container. Default: "slide"
-     */
-    @Input() slideDescription = 'slide';
-    /**
-     * The middle word for every slide's aria-label, e.g. "1 of 3". Default: "of"
-     */
-    @Input() slideLabel = 'of';
+    @HostBinding('attr.aria-label') @Input() public carouselLabel!: string;
     /**
      * The news items to be displayed as slides.
      */
-    @Input() newsItems!: NewsCarouselItem[];
+    @Input() public newsItems!: NewsCarouselItem[];
+    /**
+     * The role description for each slide container. Default: "slide"
+     */
+    @Input() public config: NewsCarouselConfig = {
+        carouselDescription: 'carousel',
+        slideDescription: 'slide',
+        slideLabel: 'of',
+        nextButtonLabel: 'Next slide',
+        previousButtonLabel: 'Previous slide',
+    }
+
+    /**
+     * The role description of the carousel container. Default: "carousel"
+     */
+    @HostBinding('attr.aria-roledescription')
+    get carouselDescription() { return this.config.carouselDescription; }
 
     @HostBinding('attr.role') role = 'region';
+
+    public activeSlide = 0;
+
+    public showPreviousSlide(): void {
+        this.activeSlide = this.activeSlide - 1 < 0 ? this.newsItems.length - 1 : this.activeSlide - 1;
+    }
+
+    public showNextSlide(): void {
+        this.activeSlide = (this.activeSlide + 1) % this.newsItems.length;
+    }
 }
--- src/app/components/news-carousel/news-carousel.interface.ts
+++ src/app/components/news-carousel/news-carousel.interface.ts
@@ -1,3 +1,20 @@
+export interface NewsCarouselConfig {
+    /**
+     * The role description of the carousel container. English default: "carousel"
+     */
+    carouselDescription: string;
+    /**
+     * The role description for each slide container. English default: "slide"
+     */
+    slideDescription: string;
+    /**
+     * The middle word for every slide's aria-label, e.g. "1 of 3". English default: "of"
+     */
+    slideLabel: string;
+    nextButtonLabel: string;
+    previousButtonLabel: string;
+}
+
 export interface NewsCarouselItem {
     heading: string;
     textSnippet: string;
